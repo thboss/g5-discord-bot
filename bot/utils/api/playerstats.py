7494ad1f7009c81f923f920ee3177967a59974bf
TheBO$$ <40491968+thboss@users.noreply.github.com>
2022-08-17 18:38:20 +0300

    /playerstats instead of /ranks

--- bot/utils/api/playerstats.py
+++ bot/utils/api/playerstats.py
@@ -11,112 +11,41 @@ class PlayerStats:
 
     def __init__(self, data) -> None:
         """"""
-        self.steam = data["steam"]
+        self.steam = data["steamId"]
         self.name = data["name"]
-        self.score = data["score"]
         self.kills = data["kills"]
         self.deaths = data["deaths"]
         self.assists = data["assists"]
-        self.suicides = data["suicides"]
-        self.tk = data["tk"]
-        self.shots = data["shots"]
-        self.hits = data["hits"]
-        self.headshots = data["headshots"]
-        self.connected = data["connected"]
-        self.rounds_tr = data["rounds_tr"]
-        self.rounds_ct = data["rounds_ct"]
-        self.knife = data["knife"]
-        self.glock = data["glock"]
-        self.hkp2000 = data["hkp2000"]
-        self.usp_silencer = data["usp_silencer"]
-        self.p250 = data["p250"]
-        self.deagle = data["deagle"]
-        self.elite = data["elite"]
-        self.fiveseven = data["fiveseven"]
-        self.tec9 = data["tec9"]
-        self.cz75a = data["cz75a"]
-        self.revolver = data["revolver"]
-        self.nova = data["nova"]
-        self.xm1014 = data["xm1014"]
-        self.mag7 = data["mag7"]
-        self.sawedoff = data["sawedoff"]
-        self.bizon = data["bizon"]
-        self.mac10 = data["mac10"]
-        self.mp9 = data["mp9"]
-        self.mp7 = data["mp7"]
-        self.ump45 = data["ump45"]
-        self.p90 = data["p90"]
-        self.galilar = data["galilar"]
-        self.ak47 = data["ak47"]
-        self.scar20 = data["scar20"]
-        self.famas = data["famas"]
-        self.m4a1 = data["m4a1"]
-        self.m4a1_silencer = data["m4a1_silencer"]
-        self.aug = data["aug"]
-        self.ssg08 = data["ssg08"]
-        self.sg556 = data["sg556"]
-        self.awp = data["awp"]
-        self.g3sg1 = data["g3sg1"]
-        self.m249 = data["m249"]
-        self.negev = data["negev"]
-        self.hegrenade = data["hegrenade"]
-        self.flashbang = data["flashbang"]
-        self.smokegrenade = data["smokegrenade"]
-        self.inferno = data["inferno"]
-        self.decoy = data["decoy"]
-        self.taser = data["taser"]
-        self.mp5sd = data["mp5sd"]
-        self.breachcharge = data["breachcharge"]
-        self.head = data["head"]
-        self.chest = data["chest"]
-        self.stomach = data["stomach"]
-        self.left_arm = data["left_arm"]
-        self.right_arm = data["right_arm"]
-        self.left_leg = data["left_leg"]
-        self.right_leg = data["right_leg"]
-        self.c4_planted = data["c4_planted"]
-        self.c4_exploded = data["c4_exploded"]
-        self.c4_defused = data["c4_defused"]
-        self.ct_win = data["ct_win"]
-        self.tr_win = data["tr_win"]
-        self.hostages_rescued = data["hostages_rescued"]
-        self.vip_killed = data["vip_killed"]
-        self.vip_escaped = data["vip_escaped"]
-        self.vip_played = data["vip_played"]
-        self.mvp = data["mvp"]
-        self.damage = data["damage"]
-        self.match_win = data["match_win"]
-        self.match_draw = data["match_draw"]
-        self.match_lose = data["match_lose"]
-        self.first_blood = data["first_blood"]
-        self.no_scope = data["no_scope"]
-        self.no_scope_dis = data["no_scope_dis"]
+        self.k1 = data["k1"]
+        self.k2 = data["k2"]
+        self.k3 = data["k3"]
+        self.k4 = data["k4"]
+        self.k5 = data["k5"]
+        self.v1 = data["v1"]
+        self.v2 = data["v2"]
+        self.v3 = data["v3"]
+        self.v4 = data["v4"]
+        self.v5 = data["v5"]
+        self.headshots = data["hsk"]
+        self.hsp = data["hsp"]
+        self.rating = data["average_rating"]
+        self.wins = data["wins"]
+        self.played = data["total_maps"]
 
     @property
     def win_percent(self):
         """"""
-        total_matches = self.match_win + self.match_draw + self.match_lose
-        return f'{self.match_win / total_matches * 100:.2f}' if total_matches else '0.00'
+        return f'{self.wins / self.played * 100:.2f}' if self.played else '0.00'
 
     @property
     def kdr(self):
         """"""
         return f'{self.kills / self.deaths:.2f}' if self.deaths else '0.00'
 
-    @property
-    def hsp(self):
-        """"""
-        return f'{self.headshots / self.kills * 100:.2f}' if self.kills else '0.00'
-
-    @property
-    def total_matches(self):
-        """"""
-        return self.match_draw + self.match_win + self.match_lose
-
     @classmethod
-    async def get_player_stats(cls, db_user, season_id=None):
+    async def get_player_stats(cls, db_user, pug=False, season_id=None):
         """"""
-        url = f"{Config.api_url}/ranks/{db_user.steam}/"
+        url = f"{Config.api_url}/playerstats/{db_user.steam}/{'pug' if pug else 'official'}/"
         if season_id:
             url += f"season/{season_id}"
 
@@ -133,10 +62,12 @@ class PlayerStats:
             raise Exception(Utils.trans('connect-api-error'))
 
     @classmethod
-    async def get_leaderboard(cls, users, season_id=None):
+    async def get_leaderboard(cls, users, pug=False, season_id=None):
         """"""
-        url = f"{Config.api_url}/ranks/"
-        if season_id:
+        url = f"{Config.api_url}/leaderboard/players/"
+        if pug:
+            url += "pug/"
+        elif season_id:
             url += f"season/{season_id}"
 
         db_steam_ids = [usr.steam for usr in users]
@@ -153,108 +84,35 @@ class PlayerStats:
                 for steam_id in db_steam_ids:
                     if steam_id not in api_steam_ids:
                         players.append({
-                            "steam": steam_id,
-                            "score": 1000,
+                            "steamId": steam_id,
                             "kills": 0,
                             "deaths": 0,
                             "assists": 0,
-                            "suicides": 0,
-                            "tk": 0,
-                            "shots": 0,
-                            "hits": 0,
-                            "headshots": 0,
-                            "connected": 0,
-                            "rounds_tr": 0,
-                            "rounds_ct": 0,
-                            "knife": 0,
-                            "glock": 0,
-                            "hkp2000": 0,
-                            "usp_silencer": 0,
-                            "p250": 0,
-                            "deagle": 0,
-                            "elite": 0,
-                            "fiveseven": 0,
-                            "tec9": 0,
-                            "cz75a": 0,
-                            "revolver": 0,
-                            "nova": 0,
-                            "xm1014": 0,
-                            "mag7": 0,
-                            "sawedoff": 0,
-                            "bizon": 0,
-                            "mac10": 0,
-                            "mp9": 0,
-                            "mp7": 0,
-                            "ump45": 0,
-                            "p90": 0,
-                            "galilar": 0,
-                            "ak47": 0,
-                            "scar20": 0,
-                            "famas": 0,
-                            "m4a1": 0,
-                            "m4a1_silencer": 0,
-                            "aug": 0,
-                            "ssg08": 0,
-                            "sg556": 0,
-                            "awp": 0,
-                            "g3sg1": 0,
-                            "m249": 0,
-                            "negev": 0,
-                            "hegrenade": 0,
-                            "flashbang": 0,
-                            "smokegrenade": 0,
-                            "inferno": 0,
-                            "decoy": 0,
-                            "taser": 0,
-                            "mp5sd": 0,
-                            "breachcharge": 0,
-                            "head": 0,
-                            "chest": 0,
-                            "stomach": 0,
-                            "left_arm": 0,
-                            "right_arm": 0,
-                            "left_leg": 0,
-                            "right_leg": 0,
-                            "c4_planted": 0,
-                            "c4_exploded": 0,
-                            "c4_defused": 0,
-                            "ct_win": 0,
-                            "tr_win": 0,
-                            "hostages_rescued": 0,
-                            "vip_killed": 0,
-                            "vip_escaped": 0,
-                            "vip_played": 0,
-                            "mvp": 0,
-                            "damage": 0,
-                            "match_win": 0,
-                            "match_draw": 0,
-                            "match_lose": 0,
-                            "first_blood": 0,
-                            "no_scope": 0,
-                            "no_scope_dis": 0
+                            "k1": 0,
+                            "k2": 0,
+                            "k3": 0,
+                            "k4": 0,
+                            "k5": 0,
+                            "v1": 0,
+                            "v2": 0,
+                            "v3": 0,
+                            "v4": 0,
+                            "v5": 0,
+                            "trp": 0,
+                            "fba": 0,
+                            "total_damage": 0,
+                            "hsk": 0,
+                            "hsp": "0.00",
+                            "average_rating": "0.00",
+                            "wins": 0,
+                            "total_maps": 0
                         })
 
-                players.sort(key=lambda x: db_steam_ids.index(x['steam']))
+                players.sort(key=lambda x: db_steam_ids.index(x['steamId']))
                 for idx, db_user in enumerate(users):
                     players[idx]['name'] = db_user.user.display_name
 
                 return [cls(player) for player in players]
-        except ContentTypeError:
-            raise Exception(Utils.trans('invalid-api-key'))
-        except (ClientConnectionError, TimeoutError):
-            raise Exception(Utils.trans('connect-api-error'))
-
-    @staticmethod
-    async def reset_stats(headers, steam_id):
-        """"""
-        url = f"{Config.api_url}/ranks/{steam_id}"
-
-        try:
-            async with Sessions.requests.delete(url=url, headers=headers) as resp:
-                if resp.status >= 400:
-                    resp_data = await resp.json()
-                    raise Exception('Response: ' + resp_data['message'])
-                return True
         except ContentTypeError:
             raise Exception(Utils.trans('invalid-api-key'))
         except (ClientConnectionError, TimeoutError):
