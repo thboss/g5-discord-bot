4798ede8f64640f0d12f7e526ca89d3b0a9b6341
TheBO$$ <40491968+thboss@users.noreply.github.com>
2023-06-17 14:11:09 +0300

    Add map_method

---
+++ migrations/20230617_01_g1wTL.py
@@ -0,0 +1,21 @@
+"""
+Add map_method
+"""
+
+from yoyo import step
+
+__depends__ = {'20220804_01_nUHpL'}
+
+steps = [
+    step(
+        'CREATE TYPE map_method AS ENUM(\'veto\', \'random\');',
+        'DROP TYPE map_method;'
+    ),
+    step(
+        (
+            'ALTER TABLE lobbies\n'
+            '    ADD COULUMN map_method map_method DEFAULT \'veto\'\n'
+            ';'
+        )
+    )
+]
