0309c70fea7e5b58c4327e4ec310be9ea24128a3
TheBO$$ <40491968+thboss@users.noreply.github.com>
2023-08-02 00:14:32 +0300

    Update __init__.py

--- bot/views/__init__.py
+++ bot/views/__init__.py
@@ -1,6 +1,5 @@
 from .readyView import ReadyView
 from .vetoView import VetoView
-from .mpoolView import MapPoolView
 from .teamsView import PickTeamsView
 from .confirmView import ConfirmView
 from .dropdownView import DropDownView
@@ -8,7 +7,6 @@ from .dropdownView import DropDownView
 __all__ = [
     ReadyView,
     VetoView,
-    MapPoolView,
     PickTeamsView,
     ConfirmView,
     DropDownView
